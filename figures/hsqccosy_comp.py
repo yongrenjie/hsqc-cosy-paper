2c84828e7cc0ad5fb2f5ada7ce4af438dc223220
Jonathan Yong <yongrenjie@gmail.com>
2024-03-25 00:22:42 +0000

    Update figures, add toc graphic

--- figures/hsqccosy_comp.py
+++ figures/hsqccosy_comp.py
@@ -6,6 +6,7 @@ apt.thesis()
 p = apt.nmrd() / '210303-7a-hsqc-cosy'
 
 dss = pg.read(p, [
+    111002,   # HSQC only
     111001,   # CLIP
     121001,   # DSE
     101001,   # TSE f=1 no relay suppression
@@ -14,19 +15,17 @@ dss = pg.read(p, [
 ])
 
 fig, axs = pg.subplots(3, 2, figsize=(5.2, 7.5))
-axs[2][1].remove(); axs = axs.flat[:5]
 
-for ds, ax in zip(dss, axs):
-    ds.stage(ax, f1_bounds=(20.5, 81), f2_bounds=(1.15, 3.31), levels=3e3)
+for i, ds, ax in apt.enzip(dss, axs.flat):
+    # For first plot, use blue for both positive and negative contours
+    # to remove the effect of multiplicity editing
+    extra_kwargs = {"colors": ("#023EFF", "#023EFF")} if i == 0 else {}
+    ds.stage(ax, f1_bounds=(20.5, 81), f2_bounds=(1.15, 3.31), levels=3e3,
+             **extra_kwargs)
     pg.mkplot(ax)
     pg.ymove(ax)
 
-pos = axs[4].get_position()
-pos.x0 = pos.x0 + 0.25
-pos.x1 = pos.x1 + 0.25
-axs[4].set_position(pos)
-
-for ax in axs[2:4]:
+for ax in axs[3:5]:
     p = dict(arrowstyle="->", color="#888")
     ax.annotate("", xy=(3.22, 37), xytext=(3.08, 32), arrowprops=p)
     ax.annotate("", xy=(1.96, 55), xytext=(2.10, 50), arrowprops=p)
